c39df8f4a6c85d4e90ca174f7d476ae787a22691
Ankit <150053943+AnkitYadav312@users.noreply.github.com>
2024-01-05 00:31:49 -0800

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,7 +3,30 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+       'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const currentVal = romanValues[s[i]];
+        const nextVal = romanValues[s[i + 1]];
+
+        if (nextVal > currentVal) {
+            result += (nextVal - currentVal);
+            i++; // Skip the next character as it is already considered in the subtraction
+        } else {
+            result += currentVal;
+        }
+    }
+
+    return result;
 };
 
 
